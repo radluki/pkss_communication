44f0acd36d9450dd6c890e48f828d0c9352ff77a
Meriadock <pmerynda@gmail.com>
2016-11-03 15:58:40 +0100

    poprawka ścieżek na lokalne

--- matapp.m
+++ matapp.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 for i=1:10
-    [status,result]=system('python client_app.py 127.0.0.1 10000 E:\pkss_communication\apptest\out1 -r Tr Tzco To -f E:\pkss_communication\apptest\client_1.input');
+    [status,result]=system('python client_app.py 127.0.0.1 10000 apptest\out1 -r Tr Tzco To -f apptest\client_1.input');
     if status==0
         result=strrep(result,'''','"');
         data=loadjson(result);
